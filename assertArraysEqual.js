dbc2a00e4c5a7e5795c56ef9a1f0e47826910fc2
DenysPyshniuk <denys.pyshniuk@gmail.com>
2020-11-25 06:07:29 +0000

    Implemented assertArraysEqual function that using previous functions

---
+++ assertArraysEqual.js
@@ -0,0 +1,30 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`✅️Assertion Passed: " ${actual} ===  ${expected}`);
+  } else if (actual !== expected) {
+    console.log(`🛑️Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const eqArrays = function (arr1, arr2) {
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function (arr1, arr2) {
+  if (eqArrays(arr1, arr2)) {
+    console.log(`Assertion Passed: ${arr1} === ${arr2}`);
+  } else {
+    console.log(`Assertion Failed: ${arr1} !== ${arr2}`);
+  }
+};
+assertArraysEqual([1, 2, 3], [1, 2, 3]);
+
+
+// TEST CODE
+//assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
