447f8b41338f82e9e9debf67599ceaba7c88b3f0
DenysPyshniuk <denys.pyshniuk@gmail.com>
2020-11-26 04:47:04 +0000

    Added some comments, finished and checked a function

--- letterPositions.js
+++ letterPositions.js
@@ -9,9 +9,9 @@ const eqArrays = function (arr1, arr2) {
 
 const assertArraysEqual = function (arr1, arr2) {
   if (eqArrays(arr1, arr2)) {
-    console.log(`Assertion Passed: ${arr1} === ${arr2}`);
+    console.log(`✅️Assertion Passed: ${arr1} === ${arr2}`);
   } else {
-    console.log(`Assertion Failed: ${arr1} !== ${arr2}`);
+    console.log(`🛑️Assertion Failed: ${arr1} !== ${arr2}`);
   }
 };
 //assertArraysEqual([1, 2, 3], [1, 2, 3]);
@@ -20,11 +20,23 @@ const assertArraysEqual = function (arr1, arr2) {
 
 
 const letterPositions = function(sentence) {
+//define var result to store return object
   const results = {};
-
-
-
-
+//for loop to go through each of sentence letters
+  for(let i = 0; i < sentence.length; i++) {
+// if letter'' is a key in sentence
+    if(results[sentence[i]]) {
+// push index to 'array of results'
+      results[sentence[i]].push(i);
+    } else {
+// else, create new key with a new arr as its value
+      results[sentence[i]] = [i];
+    }
+  }
   // logic to update results here
   return results;
 };
+//assertArraysEqual(letterPositions("hello").e, [1]);
+console.log(letterPositions("lighthouse in the house"));
+
+assertArraysEqual(letterPositions("lighthouse in the house")['o'], [6, 19]);
