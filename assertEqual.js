577b3f2e985f23df121e76d5f7f9656e6d4e7288
DenysPyshniuk <denys.pyshniuk@gmail.com>
2020-11-30 07:49:12 +0000

    esLinted

--- assertEqual.js
+++ assertEqual.js
@@ -3,7 +3,7 @@ const assertEqual = function(actual, expected) {
   //if actual is equal to expected then console log that assertion passed
   if (actual === expected) {
     console.log(`✅️Assertion Passed: " ${actual} ===  ${expected}`);
-  } else if(actual !== expected) {
+  } else if (actual !== expected) {
     //if actual is not equal to expected then console log that assertion failed
     console.log(`🛑️Assertion Failed: ${actual} !== ${expected}`);
   }
