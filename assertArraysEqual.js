ee61adcf51de7f4602c75535dbce701fdfc73bdf
DenysPyshniuk <denys.pyshniuk@gmail.com>
2020-11-30 07:17:59 +0000

    added some comments and deleted extra white space

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,12 +1,15 @@
 // FUNCTION IMPLEMENTATION
-const assertEqual = function (actual, expected) {
+//assertion of equality
+const assertEqual = function(actual, expected) {
+  //if actual is equal to expected then console log that assertion passed
   if (actual === expected) {
     console.log(`✅️Assertion Passed: " ${actual} ===  ${expected}`);
-  } else if (actual !== expected) {
+  } else if(actual !== expected) {
+  //if actual is not equal to expected then console log that assertion failed
     console.log(`🛑️Assertion Failed: ${actual} !== ${expected}`);
   }
 };
-
+//function to check if arrays are equal, returns only true or false
 const eqArrays = function (arr1, arr2) {
   for (let i = 0; i < arr1.length; i++) {
     if (arr1[i] !== arr2[i]) {
@@ -15,16 +18,14 @@ const eqArrays = function (arr1, arr2) {
   }
   return true;
 };
-
+//function to check if arrays are equal, console log that arrays are equal or not
 const assertArraysEqual = function (arr1, arr2) {
   if (eqArrays(arr1, arr2)) {
-    console.log(`Assertion Passed: ${arr1} === ${arr2}`);
+    console.log(`✅️Assertion Passed: ${arr1} === ${arr2}`);
   } else {
-    console.log(`Assertion Failed: ${arr1} !== ${arr2}`);
+    console.log(`🛑️Assertion Failed: ${arr1} !== ${arr2}`);
   }
 };
-assertArraysEqual([1, 2, 3], [1, 2, 3]);
-
 
 // TEST CODE
 //assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
