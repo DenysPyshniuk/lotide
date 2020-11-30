270877a9ae4eb4a96f7fe1103a1f0e747ea9670c
DenysPyshniuk <denys.pyshniuk@gmail.com>
2020-11-30 07:40:56 +0000

    added some comments

--- eqArrays.js
+++ eqArrays.js
@@ -1,14 +1,15 @@
-// FUNCTION IMPLEMENTATION
+//Copy "assertEqual" function
 const assertEqual = function(actual, expected) {
-
-
-  if (actual === expected) {
+  if(actual === expected) {
+    //if actual is equal to expected then console log that assertion passed
     console.log(`✅️Assertion Passed: " ${actual} ===  ${expected}`);
-  } else if (actual !== expected) {
+  } else if(actual !== expected) {
+    //if actual is not equal to expected then console log that assertion failed
     console.log(`🛑️Assertion Failed: ${actual} !== ${expected}`);
   }
 };
 
+// FUNCTION "eqArrays" IMPLEMENTATION
 const eqArrays = function(arr1, arr2) {
   for(let i = 0; i < arr1.length; i++) {
     if(arr1[i] !== arr2[i]) {
@@ -19,4 +20,4 @@ const eqArrays = function(arr1, arr2) {
 }
 
 // TEST CODE
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
+//assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
