1a0071485add37e1d48202f0738680025c6bf7ee
DenysPyshniuk <denys.pyshniuk@gmail.com>
2020-11-30 08:17:17 +0000

    esLinted

--- eqObjects.js
+++ eqObjects.js
@@ -1,12 +1,15 @@
-const assertEqual = function (actual, expected) {
+//Copy "assertEqual" function
+const assertEqual = function(actual, expected) {
   if (actual === expected) {
+    //if actual is equal to expected then console log that assertion passed
     console.log(`✅️Assertion Passed: " ${actual} ===  ${expected}`);
   } else if (actual !== expected) {
+    //if actual is not equal to expected then console log that assertion failed
     console.log(`🛑️Assertion Failed: ${actual} !== ${expected}`);
   }
 };
-
-const eqArrays = function (arr1, arr2) {
+//Copy "eqArrays" function
+const eqArrays = function(arr1, arr2) {
   for (let i = 0; i < arr1.length; i++) {
     if (arr1[i] !== arr2[i]) {
       return false;
@@ -15,37 +18,33 @@ const eqArrays = function (arr1, arr2) {
   return true;
 };
 
-// Returns true if both objects have identical keys with identical values.
-// Otherwise you get back a big fat false!
 
-const eqObjects = function (object1, object2) {
+// FUNCTION 'eqObjects' IMPLEMENTATION
+const eqObjects = function(object1, object2) {
   //if length of the objects is diffrent = false
   if (Object.keys(object1).length !== Object.keys(object2).length) return false;
-  //loop and transform object into an array
-  for (key in object1) {
+  //loop with "for in" and transform object into an array
+  for (let key in object1) {
     //Checking if array is array
     if (Array.isArray(object1[key])) {
       if (!eqArrays(object1[key], object2[key]))
         //if array NOT array than return false
         return false;
-    }
-    //if type is object than run a function
-    else if (typeof object1[key] === "object") {
+    } else if (typeof object1[key] === "object") {
+      //if type is object than return true
       return eqObjects(object1[key], object2[key]);
-    }
-    //if type is NOT an object than return false
-    else if (object1[key] !== object2[key]) return false;
+    } else if (object1[key] !== object2[key]) return false; //if type is NOT an object than return false
   }
   return true;
 };
 
-const ab = { a: "1", b: "2" };
-const ba = { b: "2", a: "1" };
-console.log(eqObjects(ab, ba)); // => true
+// const ab = { a: "1", b: "2" };
+// const ba = { b: "2", a: "1" };
+// console.log(eqObjects(ab, ba)); // => true
 
-const cd = { c: "1", d: ["2", 3] };
-const dc = { d: ["2", 3], c: "1" };
-console.log(eqObjects(cd, dc)); // => true
+// const cd = { c: "1", d: ["2", 3] };
+// const dc = { d: ["2", 3], c: "1" };
+// console.log(eqObjects(cd, dc)); // => true
 
-const cd2 = { c: "1", d: ["2", 3, 4] };
-console.log(eqObjects(cd, ba)); // => false
+// const cd2 = { c: "1", d: ["2", 3, 4] };
+// console.log(eqObjects(cd, ba)); // => false
