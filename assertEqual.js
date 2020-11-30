7a0076ae845b58f425367932c9d6efaeb4bf9b44
DenysPyshniuk <denys.pyshniuk@gmail.com>
2020-11-30 07:19:22 +0000

    added some comments

--- assertEqual.js
+++ assertEqual.js
@@ -1,12 +1,15 @@
 // FUNCTION IMPLEMENTATION
-const assertEqual = function (actual, expected) {
+//assertion of equality
+const assertEqual = function(actual, expected) {
+  //if actual is equal to expected then console log that assertion passed
   if (actual === expected) {
     console.log(`✅️Assertion Passed: " ${actual} ===  ${expected}`);
-  } else if (actual !== expected) {
+  } else if(actual !== expected) {
+    //if actual is not equal to expected then console log that assertion failed
     console.log(`🛑️Assertion Failed: ${actual} !== ${expected}`);
   }
 };
 
 // TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
+//assertEqual("Lighthouse Labs", "Bootcamp"); //=> false
+//assertEqual(1, 1); //=> true
