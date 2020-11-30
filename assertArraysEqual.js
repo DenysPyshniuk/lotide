d5491fd27bcded416b54bb1a2fa89f2e0ef0b0df
DenysPyshniuk <denys.pyshniuk@gmail.com>
2020-11-30 07:47:11 +0000

    eslinted

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -3,14 +3,14 @@ const assertEqual = function(actual, expected) {
   //if actual is equal to expected then console log that assertion passed
   if (actual === expected) {
     console.log(`✅️Assertion Passed: " ${actual} ===  ${expected}`);
-  } else if(actual !== expected) {
+  } else if (actual !== expected) {
   //if actual is not equal to expected then console log that assertion failed
     console.log(`🛑️Assertion Failed: ${actual} !== ${expected}`);
   }
 };
 
 //Copy "eqArrays" function
-const eqArrays = function (arr1, arr2) {
+const eqArrays = function(arr1, arr2) {
   for (let i = 0; i < arr1.length; i++) {
     if (arr1[i] !== arr2[i]) {
       return false;
@@ -21,7 +21,7 @@ const eqArrays = function (arr1, arr2) {
 
 // FUNCTION 'assertArraysEqual' IMPLEMENTATION
 //function to check if arrays are equal, console log that arrays are equal or not
-const assertArraysEqual = function (arr1, arr2) {
+const assertArraysEqual = function(arr1, arr2) {
   if (eqArrays(arr1, arr2)) {
     console.log(`✅️Assertion Passed: ${arr1} === ${arr2}`);
   } else {
