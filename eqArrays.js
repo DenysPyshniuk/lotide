a2244e2a1c4a1e04228beef95c1c295c3129578b
DenysPyshniuk <denys.pyshniuk@gmail.com>
2020-11-30 23:10:49 +0000

    refactored code require and exports added

--- eqArrays.js
+++ eqArrays.js
@@ -1,17 +1,5 @@
 const assertEqual = require("./assertEqual");
 
-// //Copy "assertEqual" function
-// const assertEqual = function(actual, expected) {
-//   if (actual === expected) {
-//     //if actual is equal to expected then console log that assertion passed
-//     console.log(`✅️Assertion Passed: " ${actual} ===  ${expected}`);
-//   } else if (actual !== expected) {
-//     //if actual is not equal to expected then console log that assertion failed
-//     console.log(`🛑️Assertion Failed: ${actual} !== ${expected}`);
-//   }
-// };
-
-// FUNCTION "eqArrays" IMPLEMENTATION
 const eqArrays = function(arr1, arr2) {
   for (let i = 0; i < arr1.length; i++) {
     if (arr1[i] !== arr2[i]) {
@@ -21,6 +9,4 @@ const eqArrays = function(arr1, arr2) {
   return true;
 };
 
-// TEST CODE
-//assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
 module.exports = eqArrays;
